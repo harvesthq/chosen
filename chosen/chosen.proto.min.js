923b8b8ffa0d9a19be766971285cf17dfe7b2322
Patrick Filler <patrick@getharvest.com>
2012-03-12 18:33:11 -0400

    Cake build version 0.9.8

--- chosen/chosen.proto.min.js
+++ chosen/chosen.proto.min.js
@@ -1,7 +1,7 @@
 // Chosen, a Select Box Enhancer for jQuery and Protoype
 // by Patrick Filler for Harvest, http://getharvest.com
 // 
-// Version 0.9.7
+// Version 0.9.8
 // Full source at https://github.com/harvesthq/chosen
 // Copyright (c) 2011 Harvest http://getharvest.com
 
