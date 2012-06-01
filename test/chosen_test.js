85dd6c715b7e20282c26c7e4fedd7e258f9475bb
John Paul <john@johnkpaul.com>
2012-06-01 15:19:17 -0400

    add qunit html file and first test

---
+++ test/chosen_test.js
@@ -0,0 +1,34 @@
+/*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
+/*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
+/*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
+(function($) {
+
+  /*
+    ======== A Handy Little QUnit Reference ========
+    http://docs.jquery.com/QUnit
+
+    Test methods:
+      expect(numAssertions)
+      stop(increment)
+      start(decrement)
+    Test assertions:
+      ok(value, [message])
+      equal(actual, expected, [message])
+      notEqual(actual, expected, [message])
+      deepEqual(actual, expected, [message])
+      notDeepEqual(actual, expected, [message])
+      strictEqual(actual, expected, [message])
+      notStrictEqual(actual, expected, [message])
+      raises(block, [expected], [message])
+  */
+
+  module('Chosen smoke tests', {
+    setup: function() {
+    }
+  });
+
+  test('Chosen is on the jQuery prototype object', 1, function() {
+    ok($.prototype.chosen, "The chosen function is truthy");
+  });
+
+}(jQuery));
