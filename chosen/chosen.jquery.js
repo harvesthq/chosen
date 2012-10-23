7cc0a1b7a92cd41f7c9205ab96ddef5eb3d1803a
Billy Bastardi <billy@wabism.com>
2012-10-22 22:56:32 -0400

    Fixes "hidden" containment issue due to invisible dropdown height"

--- chosen/chosen.jquery.js
+++ chosen/chosen.jquery.js
@@ -328,9 +328,9 @@ Copyright (c) 2011 by Harvest
         style: 'width: ' + this.f_width + 'px;'
       });
       if (this.is_multiple) {
-        container_div.html('<ul class="chzn-choices"><li class="search-field"><input type="text" value="' + this.default_text + '" class="default" autocomplete="off" style="width:25px;" /></li></ul><div class="chzn-drop" style="left:-9000px;"><ul class="chzn-results"></ul></div>');
+        container_div.html('<ul class="chzn-choices"><li class="search-field"><input type="text" value="' + this.default_text + '" class="default" autocomplete="off" style="width:25px;" /></li></ul><div class="chzn-drop" style="display:none;"><ul class="chzn-results"></ul></div>');
       } else {
-        container_div.html('<a href="javascript:void(0)" class="chzn-single chzn-default" tabindex="-1"><span>' + this.default_text + '</span><div><b></b></div></a><div class="chzn-drop" style="left:-9000px;"><div class="chzn-search"><input type="text" autocomplete="off" /></div><ul class="chzn-results"></ul></div>');
+        container_div.html('<a href="javascript:void(0)" class="chzn-single chzn-default" tabindex="-1"><span>' + this.default_text + '</span><div><b></b></div></a><div class="chzn-drop" style="display:none;"><div class="chzn-search"><input type="text" autocomplete="off" /></div><ul class="chzn-results"></ul></div>');
       }
       this.form_field_jq.hide().after(container_div);
       this.container = $('#' + this.container_id);
@@ -588,7 +588,8 @@ Copyright (c) 2011 by Harvest
       });
       this.dropdown.css({
         "top": dd_top + "px",
-        "left": 0
+        "left": 0,
+        "display": "block"
       });
       this.results_showing = true;
       this.search_field.focus();
@@ -605,7 +606,7 @@ Copyright (c) 2011 by Harvest
         chosen: this
       });
       this.dropdown.css({
-        "left": "-9000px"
+        "display" : "none"
       });
       return this.results_showing = false;
     };
