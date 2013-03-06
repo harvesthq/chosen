e5e5f05b625d69e8d81d5a10e23af6c153d71ce0
Patrick Filler <pf@patrickfiller.com>
2013-03-05 21:13:23 -0500

    Update the version number to 0.9.12
    - Non-retina fixes #925, #955
    - jQuery 1.9 support (removed browser) #950
    - jQuery manifest

--- chosen/chosen.jquery.min.js
+++ chosen/chosen.jquery.min.js
@@ -1,7 +1,7 @@
 // Chosen, a Select Box Enhancer for jQuery and Protoype
 // by Patrick Filler for Harvest, http://getharvest.com
 //
-// Version 0.9.11
+// Version 0.9.12
 // Full source at https://github.com/harvesthq/chosen
 // Copyright (c) 2011 Harvest http://getharvest.com
 
