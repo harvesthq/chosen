023ff23ebcfd41de922d72c962737478a76d88af
Patrick Filler <pf@patrickfiller.com>
2013-04-24 14:15:44 -0400

    Recompile again.

--- chosen/chosen.proto.js
+++ chosen/chosen.proto.js
@@ -11,6 +11,7 @@
   var SelectParser;
 
   SelectParser = (function() {
+
     function SelectParser() {
       this.options_index = 0;
       this.parsed = [];
@@ -26,7 +27,6 @@
 
     SelectParser.prototype.add_group = function(group) {
       var group_position, option, _i, _len, _ref, _results;
-
       group_position = this.parsed.length;
       this.parsed.push({
         array_index: group_position,
@@ -79,7 +79,6 @@
 
   SelectParser.select_to_array = function(select) {
     var child, parser, _i, _len, _ref;
-
     parser = new SelectParser();
     _ref = select.childNodes;
     for (_i = 0, _len = _ref.length; _i < _len; _i++) {
@@ -92,6 +91,7 @@
   this.SelectParser = SelectParser;
 
 }).call(this);
+
 /*
 Chosen source: generate output using 'cake build'
 Copyright (c) 2011 by Harvest
@@ -104,6 +104,7 @@ Copyright (c) 2011 by Harvest
   root = this;
 
   AbstractChosen = (function() {
+
     function AbstractChosen(form_field, options) {
       this.form_field = form_field;
       this.options = options != null ? options : {};
@@ -121,7 +122,6 @@ Copyright (c) 2011 by Harvest
 
     AbstractChosen.prototype.set_default_values = function() {
       var _this = this;
-
       this.click_test_action = function(evt) {
         return _this.test_active_click(evt);
       };
@@ -165,7 +165,6 @@ Copyright (c) 2011 by Harvest
 
     AbstractChosen.prototype.input_focus = function(evt) {
       var _this = this;
-
       if (this.is_multiple) {
         if (!this.active_field) {
           return setTimeout((function() {
@@ -181,7 +180,6 @@ Copyright (c) 2011 by Harvest
 
     AbstractChosen.prototype.input_blur = function(evt) {
       var _this = this;
-
       if (!this.mouse_on_container) {
         this.active_field = false;
         return setTimeout((function() {
@@ -192,7 +190,6 @@ Copyright (c) 2011 by Harvest
 
     AbstractChosen.prototype.result_add_option = function(option) {
       var classes, style;
-
       if (!option.disabled) {
         option.dom_id = this.container_id + "_o_" + option.array_index;
         classes = option.selected && this.is_multiple ? [] : ["active-result"];
@@ -240,7 +237,6 @@ Copyright (c) 2011 by Harvest
 
     AbstractChosen.prototype.keyup_checker = function(evt) {
       var stroke, _ref;
-
       stroke = (_ref = evt.which) != null ? _ref : evt.keyCode;
       this.search_field_scale();
       switch (stroke) {
@@ -277,7 +273,6 @@ Copyright (c) 2011 by Harvest
 
     AbstractChosen.prototype.generate_field_id = function() {
       var new_id;
-
       new_id = this.generate_random_id();
       this.form_field.id = new_id;
       return new_id;
@@ -285,7 +280,6 @@ Copyright (c) 2011 by Harvest
 
     AbstractChosen.prototype.generate_random_char = function() {
       var chars, newchar, rand;
-
       chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       rand = Math.floor(Math.random() * chars.length);
       return newchar = chars.substring(rand, rand + 1);
@@ -293,7 +287,6 @@ Copyright (c) 2011 by Harvest
 
     AbstractChosen.prototype.container_width = function() {
       var width;
-
       if (this.options.width != null) {
         return this.options.width;
       }
@@ -303,7 +296,6 @@ Copyright (c) 2011 by Harvest
 
     AbstractChosen.browser_is_supported = function() {
       var _ref;
-
       if (window.navigator.appName === "Microsoft Internet Explorer") {
         return (null !== (_ref = document.documentMode) && _ref >= 8);
       }
@@ -323,6 +315,7 @@ Copyright (c) 2011 by Harvest
   root.AbstractChosen = AbstractChosen;
 
 }).call(this);
+
 /*
 Chosen source: generate output using 'cake build'
 Copyright (c) 2011 by Harvest
@@ -330,18 +323,18 @@ Copyright (c) 2011 by Harvest
 
 
 (function() {
-  var Chosen, get_side_border_padding, root, _ref,
+  var Chosen, get_side_border_padding, root,
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   root = this;
 
   Chosen = (function(_super) {
+
     __extends(Chosen, _super);
 
     function Chosen() {
-      _ref = Chosen.__super__.constructor.apply(this, arguments);
-      return _ref;
+      return Chosen.__super__.constructor.apply(this, arguments);
     }
 
     Chosen.prototype.setup = function() {
@@ -364,7 +357,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.set_up_html = function() {
       var container_classes, container_props;
-
       this.container_id = this.form_field.identify().replace(/[^\w]/g, '_') + "_chzn";
       container_classes = ["chzn-container"];
       container_classes.push("chzn-container-" + (this.is_multiple ? "multi" : "single"));
@@ -409,7 +401,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.register_observers = function() {
       var _this = this;
-
       this.container.observe("mousedown", function(evt) {
         return _this.container_mousedown(evt);
       });
@@ -540,8 +531,7 @@ Copyright (c) 2011 by Harvest
     };
 
     Chosen.prototype.results_build = function() {
-      var content, data, _i, _len, _ref1;
-
+      var content, data, _i, _len, _ref;
       this.parsing = true;
       this.results_data = root.SelectParser.select_to_array(this.form_field);
       if (this.is_multiple && this.choices > 0) {
@@ -556,9 +546,9 @@ Copyright (c) 2011 by Harvest
         }
       }
       content = '';
-      _ref1 = this.results_data;
-      for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
-        data = _ref1[_i];
+      _ref = this.results_data;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        data = _ref[_i];
         if (data.group) {
           content += this.result_add_group(data);
         } else if (!data.empty) {
@@ -591,7 +581,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.result_do_highlight = function(el) {
       var high_bottom, high_top, maxHeight, visible_bottom, visible_top;
-
       this.result_clear_highlight();
       this.result_highlight = el;
       this.result_highlight.addClassName("highlighted");
@@ -644,7 +633,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.set_tab_index = function(el) {
       var ti;
-
       if (this.form_field.tabIndex) {
         ti = this.form_field.tabIndex;
         this.form_field.tabIndex = -1;
@@ -664,7 +652,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.search_results_mouseup = function(evt) {
       var target;
-
       target = evt.target.hasClassName("active-result") ? evt.target : evt.target.up(".active-result");
       if (target) {
         this.result_highlight = target;
@@ -675,7 +662,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.search_results_mouseover = function(evt) {
       var target;
-
       target = evt.target.hasClassName("active-result") ? evt.target : evt.target.up(".active-result");
       if (target) {
         return this.result_do_highlight(target);
@@ -698,7 +684,6 @@ Copyright (c) 2011 by Harvest
     Chosen.prototype.choice_build = function(item) {
       var choice_id, link,
         _this = this;
-
       if (this.is_multiple && this.max_selected_options <= this.choices) {
         this.form_field.fire("liszt:maxselected", {
           chosen: this
@@ -760,7 +745,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.results_reset_cleanup = function() {
       var deselect_trigger;
-
       this.current_selectedIndex = this.form_field.selectedIndex;
       deselect_trigger = this.selected_item.down("abbr");
       if (deselect_trigger) {
@@ -770,7 +754,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.result_select = function(evt) {
       var high, item, position;
-
       if (this.result_highlight) {
         high = this.result_highlight;
         this.result_clear_highlight();
@@ -816,7 +799,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.result_deselect = function(pos) {
       var result, result_data;
-
       result_data = this.results_data[pos];
       if (!this.form_field.options[result_data.options_index].disabled) {
         result_data.selected = false;
@@ -844,17 +826,16 @@ Copyright (c) 2011 by Harvest
     };
 
     Chosen.prototype.winnow_results = function() {
-      var found, option, part, parts, regex, regexAnchor, result_id, results, searchText, startpos, text, zregex, _i, _j, _len, _len1, _ref1;
-
+      var found, option, part, parts, regex, regexAnchor, result_id, results, searchText, startpos, text, zregex, _i, _j, _len, _len1, _ref;
       this.no_results_clear();
       results = 0;
       searchText = this.search_field.value === this.default_text ? "" : this.search_field.value.strip().escapeHTML();
       regexAnchor = this.search_contains ? "" : "^";
       regex = new RegExp(regexAnchor + searchText.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&"), 'i');
       zregex = new RegExp(searchText.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&"), 'i');
-      _ref1 = this.results_data;
-      for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
-        option = _ref1[_i];
+      _ref = this.results_data;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        option = _ref[_i];
         if (!option.disabled && !option.empty) {
           if (option.group) {
             $(option.dom_id).hide();
@@ -911,7 +892,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.winnow_results_clear = function() {
       var li, lis, _i, _len, _results;
-
       this.search_field.clear();
       lis = this.search_results.select("li");
       _results = [];
@@ -930,12 +910,11 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.winnow_results_set_highlight = function() {
       var do_high;
-
       if (!this.result_highlight) {
         if (!this.is_multiple) {
           do_high = this.search_results.down(".result-selected.active-result");
         }
-        if (do_high == null) {
+        if (!(do_high != null)) {
           do_high = this.search_results.down(".active-result");
         }
         if (do_high != null) {
@@ -952,7 +931,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.no_results_clear = function() {
       var nr, _results;
-
       nr = null;
       _results = [];
       while (nr = this.search_results.down(".no-results")) {
@@ -963,7 +941,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.keydown_arrow = function() {
       var actives, nexts, sibs;
-
       actives = this.search_results.select("li.active-result");
       if (actives.length) {
         if (!this.result_highlight) {
@@ -983,7 +960,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.keyup_arrow = function() {
       var actives, prevs, sibs;
-
       if (!this.results_showing && !this.is_multiple) {
         return this.results_show();
       } else if (this.result_highlight) {
@@ -1003,7 +979,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.keydown_backstroke = function() {
       var next_available_destroy;
-
       if (this.pending_backstroke) {
         this.choice_destroy(this.pending_backstroke.down("a"));
         return this.clear_backstroke();
@@ -1031,9 +1006,8 @@ Copyright (c) 2011 by Harvest
     };
 
     Chosen.prototype.keydown_checker = function(evt) {
-      var stroke, _ref1;
-
-      stroke = (_ref1 = evt.which) != null ? _ref1 : evt.keyCode;
+      var stroke, _ref;
+      stroke = (_ref = evt.which) != null ? _ref : evt.keyCode;
       this.search_field_scale();
       if (stroke !== 8 && this.pending_backstroke) {
         this.clear_backstroke();
@@ -1063,7 +1037,6 @@ Copyright (c) 2011 by Harvest
 
     Chosen.prototype.search_field_scale = function() {
       var div, h, style, style_block, styles, w, _i, _len;
-
       if (this.is_multiple) {
         h = 0;
         w = 0;
@@ -1099,7 +1072,6 @@ Copyright (c) 2011 by Harvest
 
   get_side_border_padding = function(elmt) {
     var layout, side_border_padding;
-
     layout = new Element.Layout(elmt);
     return side_border_padding = layout.get("border-left") + layout.get("border-right") + layout.get("padding-left") + layout.get("padding-right");
   };
