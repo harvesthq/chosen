70af5c11af1c9315335046a9dab0930881832285
Patrick Filler <pf@patrickfiller.com>
2013-06-03 17:17:59 -0400

    fixed onkeypress event for firefox

--- chosen/chosen.jquery.min.js
+++ chosen/chosen.jquery.min.js
@@ -1,7 +1,7 @@
 // Chosen, a Select Box Enhancer for jQuery and Prototype
 // by Patrick Filler for Harvest, http://getharvest.com
 //
-// Version 0.9.14
+// Version 0.9.15
 // Full source at https://github.com/harvesthq/chosen
 // Copyright (c) 2011 Harvest http://getharvest.com
 
