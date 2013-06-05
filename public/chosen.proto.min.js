daf8f366bbcbb907d249da4f6a170a25a172640e
Ken Earley <ken@getharvest.com>
2013-06-05 15:45:54 -0500

    Committing built files

--- public/chosen.proto.min.js
+++ public/chosen.proto.min.js
@@ -1,7 +1,7 @@
-// Chosen, a Select Box Enhancer for jQuery and Protoype
+// Chosen, a Select Box Enhancer for jQuery and Prototype
 // by Patrick Filler for Harvest, http://getharvest.com
 //
-// Version 0.9.15
+// Version 0.9.17
 // Full source at https://github.com/harvesthq/chosen
 // Copyright (c) 2011 Harvest http://getharvest.com
 
