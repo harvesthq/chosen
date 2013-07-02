ac8c7e1b18d6d1766e420e3e5f0f879110d26238
Patrick Filler <pf@patrickfiller.com>
2013-07-02 17:20:14 -0400

    Build: 2 Branch: master  0.11.1 SHA: ff94a4b

--- chosen.proto.js
+++ chosen.proto.js
@@ -1,7 +1,7 @@
 // Chosen, a Select Box Enhancer for jQuery and Prototype
 // by Patrick Filler for Harvest, http://getharvest.com
 //
-// Version 0.10.0
+// Version 0.11.1
 // Full source at https://github.com/harvesthq/chosen
 // Copyright (c) 2011 Harvest http://getharvest.com
 
@@ -26,6 +26,7 @@
 
     SelectParser.prototype.add_group = function(group) {
       var group_position, option, _i, _len, _ref, _results;
+
       group_position = this.parsed.length;
       this.parsed.push({
         array_index: group_position,
@@ -78,6 +79,7 @@
 
   SelectParser.select_to_array = function(select) {
     var child, parser, _i, _len, _ref;
+
     parser = new SelectParser();
     _ref = select.childNodes;
     for (_i = 0, _len = _ref.length; _i < _len; _i++) {
@@ -114,6 +116,7 @@
 
     AbstractChosen.prototype.set_default_values = function() {
       var _this = this;
+
       this.click_test_action = function(evt) {
         return _this.test_active_click(evt);
       };
@@ -156,6 +159,7 @@
 
     AbstractChosen.prototype.input_focus = function(evt) {
       var _this = this;
+
       if (this.is_multiple) {
         if (!this.active_field) {
           return setTimeout((function() {
@@ -171,6 +175,7 @@
 
     AbstractChosen.prototype.input_blur = function(evt) {
       var _this = this;
+
       if (!this.mouse_on_container) {
         this.active_field = false;
         return setTimeout((function() {
@@ -181,6 +186,7 @@
 
     AbstractChosen.prototype.result_add_option = function(option) {
       var classes, style;
+
       option.dom_id = this.container_id + "_o_" + option.array_index;
       classes = [];
       if (!option.disabled && !(option.selected && this.is_multiple)) {
@@ -230,6 +236,7 @@
 
     AbstractChosen.prototype.choices_count = function() {
       var option, _i, _len, _ref;
+
       if (this.selected_option_count != null) {
         return this.selected_option_count;
       }
@@ -253,6 +260,7 @@
 
     AbstractChosen.prototype.keyup_checker = function(evt) {
       var stroke, _ref;
+
       stroke = (_ref = evt.which) != null ? _ref : evt.keyCode;
       this.search_field_scale();
       switch (stroke) {
@@ -289,6 +297,7 @@
 
     AbstractChosen.prototype.generate_field_id = function() {
       var new_id;
+
       new_id = this.generate_random_id();
       this.form_field.id = new_id;
       return new_id;
@@ -296,6 +305,7 @@
 
     AbstractChosen.prototype.generate_random_char = function() {
       var chars, newchar, rand;
+
       chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       rand = Math.floor(Math.random() * chars.length);
       return newchar = chars.substring(rand, rand + 1);
@@ -311,6 +321,7 @@
 
     AbstractChosen.browser_is_supported = function() {
       var _ref;
+
       if (window.navigator.appName === "Microsoft Internet Explorer") {
         return (null !== (_ref = document.documentMode) && _ref >= 8);
       }
@@ -364,6 +375,7 @@
 
     Chosen.prototype.set_up_html = function() {
       var container_classes, container_props;
+
       this.container_id = this.form_field.identify().replace(/[^\w]/g, '_') + "_chzn";
       container_classes = ["chzn-container"];
       container_classes.push("chzn-container-" + (this.is_multiple ? "multi" : "single"));
@@ -409,6 +421,7 @@
 
     Chosen.prototype.register_observers = function() {
       var _this = this;
+
       this.container.observe("mousedown", function(evt) {
         return _this.container_mousedown(evt);
       });
@@ -514,6 +527,7 @@
 
     Chosen.prototype.search_results_mousewheel = function(evt) {
       var delta;
+
       delta = -evt.wheelDelta || evt.detail;
       if (delta != null) {
         evt.preventDefault();
@@ -557,6 +571,7 @@
 
     Chosen.prototype.results_build = function() {
       var content, data, _i, _len, _ref1;
+
       this.parsing = true;
       this.selected_option_count = null;
       this.results_data = root.SelectParser.select_to_array(this.form_field);
@@ -604,6 +619,7 @@
 
     Chosen.prototype.result_do_highlight = function(el) {
       var high_bottom, high_top, maxHeight, visible_bottom, visible_top;
+
       this.result_clear_highlight();
       this.result_highlight = el;
       this.result_highlight.addClassName("highlighted");
@@ -656,6 +672,7 @@
 
     Chosen.prototype.set_tab_index = function(el) {
       var ti;
+
       if (this.form_field.tabIndex) {
         ti = this.form_field.tabIndex;
         this.form_field.tabIndex = -1;
@@ -665,6 +682,7 @@
 
     Chosen.prototype.set_label_behavior = function() {
       var _this = this;
+
       this.form_field_label = this.form_field.up("label");
       if (this.form_field_label == null) {
         this.form_field_label = $$("label[for='" + this.form_field.id + "']").first();
@@ -692,6 +710,7 @@
 
     Chosen.prototype.search_results_mouseup = function(evt) {
       var target;
+
       target = evt.target.hasClassName("active-result") ? evt.target : evt.target.up(".active-result");
       if (target) {
         this.result_highlight = target;
@@ -702,6 +721,7 @@
 
     Chosen.prototype.search_results_mouseover = function(evt) {
       var target;
+
       target = evt.target.hasClassName("active-result") ? evt.target : evt.target.up(".active-result");
       if (target) {
         return this.result_do_highlight(target);
@@ -717,6 +737,7 @@
     Chosen.prototype.choice_build = function(item) {
       var choice, close_link,
         _this = this;
+
       choice = new Element('li', {
         "class": "search-choice"
       }).update("<span>" + item.html + "</span>");
@@ -776,6 +797,7 @@
 
     Chosen.prototype.results_reset_cleanup = function() {
       var deselect_trigger;
+
       this.current_selectedIndex = this.form_field.selectedIndex;
       deselect_trigger = this.selected_item.down("abbr");
       if (deselect_trigger) {
@@ -785,6 +807,7 @@
 
     Chosen.prototype.result_select = function(evt) {
       var high, item, position;
+
       if (this.result_highlight) {
         high = this.result_highlight;
         this.result_clear_highlight();
@@ -845,6 +868,7 @@
 
     Chosen.prototype.result_deselect = function(pos) {
       var result, result_data;
+
       result_data = this.results_data[pos];
       if (!this.form_field.options[result_data.options_index].disabled) {
         result_data.selected = false;
@@ -878,6 +902,7 @@
 
     Chosen.prototype.winnow_results = function() {
       var found, option, part, parts, regex, regexAnchor, result_id, results, searchText, startpos, text, zregex, _i, _j, _len, _len1, _ref1;
+
       this.no_results_clear();
       results = 0;
       searchText = this.search_field.value === this.default_text ? "" : this.search_field.value.strip().escapeHTML();
@@ -943,6 +968,7 @@
 
     Chosen.prototype.winnow_results_set_highlight = function() {
       var do_high;
+
       if (!this.result_highlight) {
         if (!this.is_multiple) {
           do_high = this.search_results.down(".result-selected.active-result");
@@ -964,6 +990,7 @@
 
     Chosen.prototype.no_results_clear = function() {
       var nr, _results;
+
       nr = null;
       _results = [];
       while (nr = this.search_results.down(".no-results")) {
@@ -974,6 +1001,7 @@
 
     Chosen.prototype.keydown_arrow = function() {
       var next_sib;
+
       if (this.results_showing && this.result_highlight) {
         next_sib = this.result_highlight.next('.active-result');
         if (next_sib) {
@@ -986,6 +1014,7 @@
 
     Chosen.prototype.keyup_arrow = function() {
       var actives, prevs, sibs;
+
       if (!this.results_showing && !this.is_multiple) {
         return this.results_show();
       } else if (this.result_highlight) {
@@ -1005,6 +1034,7 @@
 
     Chosen.prototype.keydown_backstroke = function() {
       var next_available_destroy;
+
       if (this.pending_backstroke) {
         this.choice_destroy(this.pending_backstroke.down("a"));
         return this.clear_backstroke();
@@ -1033,6 +1063,7 @@
 
     Chosen.prototype.keydown_checker = function(evt) {
       var stroke, _ref1;
+
       stroke = (_ref1 = evt.which) != null ? _ref1 : evt.keyCode;
       this.search_field_scale();
       if (stroke !== 8 && this.pending_backstroke) {
@@ -1056,6 +1087,7 @@
           this.keyup_arrow();
           break;
         case 40:
+          evt.preventDefault();
           this.keydown_arrow();
           break;
       }
@@ -1063,6 +1095,7 @@
 
     Chosen.prototype.search_field_scale = function() {
       var div, h, style, style_block, styles, w, _i, _len;
+
       if (this.is_multiple) {
         h = 0;
         w = 0;
