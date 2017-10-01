7a95f35acd01213ea3460e756ecd73e4e6bd28af
Mikk3lRo <mikk3lro@gmail.com>
2017-10-01 16:46:28 +0200

    Update Documentation and Examples
    Added descriptions of the new search-behaviour related options to the
    options page, as well as a testing area on the examples-page where the
    effects of different combinations of settings can be easily previewed.

--- public/docsupport/init.js
+++ public/docsupport/init.js
@@ -9,3 +9,29 @@ var config = {
 for (var selector in config) {
   $(selector).chosen(config[selector]);
 }
+
+function set_search_behaviour() {
+    var options = {};
+    $("[data-search-behaviour]").each(function(){
+        var setting = $(this).data('search-behaviour');
+        options[setting] = $('[name=' + this.name + ']:checked').val();
+        var stringified = false;
+        if (options[setting] === 'true') {
+            options[setting] = true;
+        } else if (options[setting] === 'false') {
+            options[setting] = false;
+        } else if (options[setting] === 'regex_space') {
+            options[setting] = /\s/;
+        } else {
+            stringified = JSON.stringify(options[setting]);
+        }
+        code_elm = $(this).closest('div').find('> label > code');
+        code_elm.html(setting + ': ' + (stringified ? stringified : options[setting].toString()));
+        Prism.highlightElement(code_elm[0]);
+    });
+    
+    $('.chosen-select-sb').chosen("destroy").chosen(options);
+}
+$(document).on('change', "[data-search-behaviour]", set_search_behaviour);
+
+set_search_behaviour();
