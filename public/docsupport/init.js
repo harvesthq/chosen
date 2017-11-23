40ef15f4d9e25572ee980ce233bd131a3f8a39eb
Ariel Barreiro <abarrei@gmail.com>
2017-11-23 16:32:03 -0300

    Support for trasliteration/normalization

--- public/docsupport/init.js
+++ public/docsupport/init.js
@@ -1,10 +1,11 @@
 var config = {
-  '.chosen-select'           : {},
-  '.chosen-select-deselect'  : { allow_single_deselect: true },
-  '.chosen-select-no-single' : { disable_search_threshold: 10 },
-  '.chosen-select-no-results': { no_results_text: 'Oops, nothing found!' },
-  '.chosen-select-rtl'       : { rtl: true },
-  '.chosen-select-width'     : { width: '95%' }
+  '.chosen-select'                 : {},
+  '.chosen-select-deselect'        : { allow_single_deselect: true },
+  '.chosen-select-no-single'       : { disable_search_threshold: 10 },
+  '.chosen-select-no-results'      : { no_results_text: 'Oops, nothing found!' },
+  '.chosen-select-rtl'             : { rtl: true },
+  '.chosen-select-width'           : { width: '95%' },
+  '.chosen-select-transliteration' : { normalize_search_text: transl, search_contains: true }
 }
 for (var selector in config) {
   $(selector).chosen(config[selector]);
--- public/docsupport/init.proto.js
+++ public/docsupport/init.proto.js
@@ -1,13 +1,14 @@
 document.observe('dom:loaded', function(evt) {
   var config = {
-    '.chosen-select'           : {},
-    '.chosen-select-deselect'  : { allow_single_deselect: true },
-    '.chosen-select-no-single' : { disable_search_threshold: 10 },
-    '.chosen-select-no-results': { no_results_text: 'Oops, nothing found!' },
-    '.chosen-select-rtl'       : { rtl: true },
-    '.chosen-select-width'     : { width: '95%' }
+    '.chosen-select'                 : {},
+    '.chosen-select-deselect'        : { allow_single_deselect: true },
+    '.chosen-select-no-single'       : { disable_search_threshold: 10 },
+    '.chosen-select-no-results'      : { no_results_text: 'Oops, nothing found!' },
+    '.chosen-select-rtl'             : { rtl: true },
+    '.chosen-select-width'           : { width: '95%' },
+    '.chosen-select-transliteration' : { normalize_search_text: transl, search_contains: true }
   }
-  
+
   for (var selector in config) {
     $$(selector).each(function(element) {
       new Chosen(element, config[selector]);
